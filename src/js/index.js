f972d03a506fb603a7d51be9d0e9d69930714418
Linda-Ikechukwu <lindaedwin96@gmail.com>
2019-07-03 00:41:22 +0100

    minor changes

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+//Registering the service worker
+if ('serviceWorker' in navigator) {
+    
+    navigator.serviceWorker.register('../sw.js').then(registration => {
+      // Registration was successful
+     console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      //registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  
+}
