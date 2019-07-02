ca19a629454fd58d61d3bf96573f7d8f8ffee66a
Linda-Ikechukwu <lindaedwin96@gmail.com>
2019-07-02 22:26:44 +0100

    minor changes

--- js/highscores.js
+++
@@ -1,6 +0,0 @@
-const highScoreList = document.getElementById('high-score-list');
-const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
-
-highScoreList.innerHTML = highScores.map(highscore =>{
-    return  `<li class="high-score">${highscore.username} - ${highscore.score}</li>`
-}).join('');
